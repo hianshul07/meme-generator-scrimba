40bcde51b5476086c89de79d4b9336673af2b368
hianshul07 <hianshul07@gmail.com>
2024-01-25 23:35:11 +0530

    images added with state

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,7 +1,21 @@
+import { useState } from 'react';
+import memesData from '../memesData';
+
 const Meme = () => {
+
+  const [memeImage, setMemeImage] = useState('')
+
+  const getMemeData = () => {
+    const memesArray = memesData.data.memes;
+    const randomData = Math.floor(Math.random() * memesArray.length)
+    const url = memesArray[randomData].url
+    // console.log(url);
+    setMemeImage(url)
+  };
+
   return (
-    <div className="flex justify-center">
-      <form className='w-5/6 flex flex-col px-96 py-16 gap-8'>
+    <div className='flex items-center'>
+      <div className='flex flex-col px-96 py-16 gap-8 items-center'>
         <div className='flex justify-between gap-12'>
           <input
             type='text'
@@ -14,10 +28,14 @@ const Meme = () => {
             placeholder='upper text'
           />
         </div>
-        <button className='bg-[#6b2385] text-white rounded-md font-semibold bold text-2xl px-4 py-4'>
+        <button
+          onClick={getMemeData}
+          className='bg-[#6b2385] w-full text-white rounded-md font-semibold bold text-2xl px-4 py-4'
+        >
           Get a new meme image 🖼️
         </button>
-      </form>
+        <img className='h-fit w-fit' src={memeImage} alt="" />
+      </div>
     </div>
   );
 };
